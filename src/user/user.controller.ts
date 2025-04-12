4f4e3347fbd81c3ab7b94fdda47cbedbae891616
zhenghui-su <343196323@qq.com>
2025-04-13 00:35:59 +0800

    feat(user): 添加用户密码更新和用户信息更新接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -18,6 +18,8 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { RequireLogin, UserInfo } from 'src/custom.decorator';
 import { UserDetailVo } from './vo/user-info.vo';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/udpate-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -201,6 +203,59 @@ export class UserController {
 
     return vo;
   }
+  @Post(['update_password', 'admin/update_password'])
+  @RequireLogin()
+  async updatePassword(
+    @UserInfo('userId') userId: number,
+    @Body() passwordDto: UpdateUserPasswordDto,
+  ) {
+    return await this.userService.updatePassword(userId, passwordDto);
+  }
+  @Get('update_password/captcha')
+  async updatePasswordCaptcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(
+      `update_password_captcha_${address}`,
+      code,
+      10 * 60,
+    );
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改密码验证码',
+      html: `<p>你的更改密码验证码是 ${code}</p>`,
+    });
+    return '发送成功';
+  }
+
+  @Post(['update', 'admin/update'])
+  @RequireLogin()
+  async update(
+    @UserInfo('userId') userId: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.userService.update(userId, updateUserDto);
+  }
+
+  @Get('update/captcha')
+  async updateCaptcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(
+      `update_user_captcha_${address}`,
+      code,
+      60 * 10,
+    );
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更新用户信息验证码',
+      html: `<p>您的验证码是：${code}</p>`,
+    });
+    return '发送成功';
+  }
+
   @Get('init-data')
   async initData() {
     await this.userService.initData();
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -15,6 +15,8 @@ import { Role } from './entities/role.entity';
 import { Permission } from './entities/permission.entity';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoginUserVo } from './vo/login-user.vo';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/udpate-user.dto';
 
 @Injectable()
 export class UserService {
@@ -122,6 +124,59 @@ export class UserService {
     });
 
     return user;
+  }
+  async updatePassword(userId: number, passwordDto: UpdateUserPasswordDto) {
+    const captcha = await this.redisService.get(
+      `update_password_captcha_${passwordDto.email}`,
+    );
+
+    if (!captcha)
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+
+    if (passwordDto.captcha !== captcha)
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+
+    const foundUser = await this.userRepository.findOneBy({
+      id: userId,
+    });
+    if (!foundUser)
+      throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST);
+    foundUser.password = md5(passwordDto.password);
+
+    try {
+      await this.userRepository.save(foundUser);
+      return '密码修改成功';
+    } catch (e) {
+      this.logger.error(e, UserService);
+      return '密码修改失败';
+    }
+  }
+
+  async update(userId: number, updateUserDto: UpdateUserDto) {
+    const captcha = await this.redisService.get(
+      `update_user_captcha_${updateUserDto.email}`,
+    );
+    if (!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+    }
+    if (updateUserDto.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+    const foundUser = await this.userRepository.findOneBy({
+      id: userId,
+    });
+    if (!foundUser)
+      throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST);
+    if (updateUserDto.nickName) foundUser.nickName = updateUserDto.nickName;
+    if (updateUserDto.headPic) foundUser.headPic = updateUserDto.headPic;
+
+    try {
+      await this.userRepository.save(foundUser);
+      return '用户信息修改成功';
+    } catch (e) {
+      this.logger.error(e, UserService);
+      return '用户信息修改失败';
+    }
   }
   // 初始化数据
   async initData() {
